e4086e8aa973e05da289bde1e94c1aa552b65bf2
xl <xl@qq.com>
2021-06-12 17:08:48 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
-   
+    <router-view></router-view>
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
+import MainTabBar from "components/content/maintabbar/MainTabBar.vue"
+
 export default {
-  name: 'App'
+  name: "App",
+  components: {
+    MainTabBar
+  }
 }
 </script>
 
 <style>
+@import url("assets/css/base.css");
 
 </style>
---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="tab-bar">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "TabBar"
+}
+</script>
+
+<style scoped>
+	#tab-bar {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    background-color: #eee;
+    box-shadow: 0 -1px 10px rgba(0, 0, 0, 0.1);
+		height: 49px;
+  }
+</style>
